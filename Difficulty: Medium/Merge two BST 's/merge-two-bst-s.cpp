db198a57ed23f167afbe19b26e7d7afa8c840a2b
Sairudra More <sairudra60@gmail.com>
2025-10-25 18:31:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -0,0 +1,45 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> merge(Node *root1, Node *root2) {
+        vector<int> ans; // stores sorted elements of both the trees
+        stack<Node*> sa, sb;
+        Node *a = root1, *b = root2;
+        while (a || b || !sa.empty() || !sb.empty()) {
+            while (a) {
+                sa.push(a);
+                a = a->left;
+            }
+
+            while (b) {
+                sb.push(b);
+                b = b->left;
+            }
+
+            if (sb.empty() || (!sa.empty() && sa.top()->data <= sb.top()->data)) {
+                auto atop = sa.top();
+                ans.push_back(atop->data);
+                sa.pop();
+                a = atop->right;
+            } else {
+                auto btop = sb.top();
+                sb.pop();
+                ans.push_back(btop->data);
+                b = btop->right;
+            }
+        }
+        return ans;
+    }
+};
