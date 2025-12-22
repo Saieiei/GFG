96f50031f556541580c514cab32378da82c30969
Sairudra More <sairudra60@gmail.com>
2025-12-22 16:40:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -1,26 +1,32 @@
-//M2 Memoization
+//M4 SO
 //this is more mathematical => u will get the recursion formula and base case conditions
 class Solution {
   public:
   
-    int recursionMemo(int n, int k, vector<int>& dp){
-        //basecase
-        //3. check if its there in dp
-        if(n==1) return k;
-        if(n==2) return k*k;
-        if(dp[n] != -1)  return dp[n];
+    int recursionTabuSO(int n, int k){
+        //1. create and update dp with BC
+        if(n == 1) return k;
+        if(n == 2) return k*k;
+        
+        int curr;
+        int prev2 = k;
+        int prev1 = k*k;
         
         //recursion formula
-        //2. save the res in dp
+        //2. forloop, reverse, copy-paste, rec-fun, indexing
+        for(int N=3; N<=n; N++){
+            curr =  ((prev1 + prev2) * (k-1));
+            
+            //exchnage
+            prev2 = prev1;
+            prev1 = curr;
+        }
         
-        dp[n] =  ((recursionMemo(n-1, k, dp) + recursionMemo(n-2, k, dp)) * (k-1));
-        return dp[n];
+        //3. return accordingly
+        return curr;
     }
   
     int countWays(int n, int k) {
-        //1. create dp
-        vector<int>dp(n+1, -1);
-        return recursionMemo(n, k, dp);
-        
+        return recursionTabuSO(n, k);
     }
 };
