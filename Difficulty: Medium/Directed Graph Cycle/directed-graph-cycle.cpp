c2f1fbc4000113c28754f9f0ddfa3c5f9795a801
Sairudra More <sairudra60@gmail.com>
2026-02-02 21:47:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,45 +1,60 @@
-//we will have to keep the track of the recursion path and visited or not
+//use Topological sort using BFS
+    //1st create AdjList
+    //2nd create and keep track of indegrees
+    //3rd keep performing until the q is empty
+//based on the ans size return 
+
 class Solution {
   public:
-    bool isCyclicDFS(int src, unordered_map<int, bool>& visted, unordered_map<int, bool>& DFSTracker, unordered_map<int, vector<int>>& adjList){
-        visted[src] = true;
-        DFSTracker[src] = true;
-        for(int nbr: adjList[src]){
-            if(!visted[nbr]){
-                bool ans = isCyclicDFS(nbr, visted, DFSTracker, adjList);
-                if(ans == true) return true;
-            }
-            else{//visited but check if same dfs path is same using the tracker
-                if(DFSTracker[nbr] == true) return true;
-            }
-        }
-        //backtracking
-        DFSTracker[src] = false;
-        return false;
-    }
-    bool isCyclicHelper(int V, unordered_map<int, vector<int>>& adjList){
-        unordered_map<int, bool> visted;
-        unordered_map<int, bool> DFSTracker;
-        //it can have discontinued components
-        for(int src=0; src < V; src++){
-            if(!visted[src]){
-                bool ans = isCyclicDFS(src, visted, DFSTracker, adjList);
-                if(ans == true) return true; //cycle found means return there only
-            }
-        }
-        return false; //no cycle
-    }
-    bool isCyclic(int V, vector<vector<int>> &edges) {
-        //1st create adjList
-        unordered_map<int, vector<int>>adjList;
+    void solveBFS(int& V, vector<vector<int>>& edges, unordered_map<int, vector<int>>& AdjList, vector<int>& ans){
+        //2nd create and keep track of indegrees
+        unordered_map<int, int> Indegrees;
+        //initialise all the indegrees to 0
+        //for(int i = 0; i < V; i++){
+        //    Indegrees[i] = 0;
+        //}
+        //track indegrees of each node
         for(vector<int> edge: edges){
             int u = edge[0];
             int v = edge[1];
-            //since its directed only u->v
-            adjList[u].push_back(v);
+            //u->v, v will be tracked for the indegrees
+            Indegrees[v]++;
         }
-        
-        return isCyclicHelper(V, adjList);
+        //push the 0 indegree inside the q
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(Indegrees[i] == 0){
+                q.push(i);
+            }
+        }
+        //keep on executing until q is empty
+        while(!q.empty()){
+            int frontEle = q.front();
+            q.pop();
+            ans.push_back(frontEle);
+            //update the indegree
+            for(int nbr: AdjList[frontEle]){
+                Indegrees[nbr]--;
+                if(Indegrees[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+    }
+    bool isCyclic(int V, vector<vector<int>>& edges) {
+        //1st create AdjList
+        unordered_map<int, vector<int>> AdjList;
+        for(vector<int> edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            //u->v (directed)
+            AdjList[u].push_back(v);
+        }
+        //lets start the process now
+        vector<int> ans;
+        solveBFS(V, edges, AdjList, ans);
+        if(ans.size() == V)return false;
+        if(ans.size() < V) return true;
         
     }
 };
