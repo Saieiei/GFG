d9807fe3986c39d127d2dc564f1f55d4718f1e84
Sairudra More <sairudra60@gmail.com>
2026-04-17 23:30:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,60 +1,47 @@
-//use Topological sort using BFS
-    //1st create AdjList
-    //2nd create and keep track of indegrees
-    //3rd keep performing until the q is empty
-//based on the ans size return 
-
 class Solution {
   public:
-    void solveBFS(int& V, vector<vector<int>>& edges, unordered_map<int, vector<int>>& AdjList, vector<int>& ans){
-        //2nd create and keep track of indegrees
-        unordered_map<int, int> Indegrees;
-        //initialise all the indegrees to 0
-        //for(int i = 0; i < V; i++){
-        //    Indegrees[i] = 0;
-        //}
-        //track indegrees of each node
+  
+    bool isCycleDFS(int i, vector<int>& isVisited, vector<int>&isPathVisited, vector<vector<int>>& adjList){
+        //mark it as visited
+        isVisited[i] = true;
+        isPathVisited[i] = true;
+        
+        //explore nbrs
+        for(int nbr: adjList[i]){
+            if(!isVisited[nbr]){
+                if(isCycleDFS(nbr, isVisited, isPathVisited, adjList)){
+                    return true;
+                }
+            }
+            else if(isPathVisited[nbr] == true){
+                return true;
+            }
+        }
+        isPathVisited[i] = false;
+        return false;
+    }
+  
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        //adjList
+        vector<vector<int>> adjList(V);
         for(vector<int> edge: edges){
             int u = edge[0];
             int v = edge[1];
-            //u->v, v will be tracked for the indegrees
-            Indegrees[v]++;
+            //directed u->v
+            adjList[u].push_back(v);
         }
-        //push the 0 indegree inside the q
-        queue<int> q;
-        for(int i = 0; i < V; i++){
-            if(Indegrees[i] == 0){
-                q.push(i);
-            }
-        }
-        //keep on executing until q is empty
-        while(!q.empty()){
-            int frontEle = q.front();
-            q.pop();
-            ans.push_back(frontEle);
-            //update the indegree
-            for(int nbr: AdjList[frontEle]){
-                Indegrees[nbr]--;
-                if(Indegrees[nbr] == 0){
-                    q.push(nbr);
+        
+        vector<int> isVisited(V, false);
+        vector<int> isPathVisited(V, false);
+        
+        //disconnected graph
+        for(int i=0; i<V; i++){
+            if(!isVisited[i]){
+                if(isCycleDFS(i, isVisited, isPathVisited, adjList)){
+                    return true;
                 }
             }
         }
-    }
-    bool isCyclic(int V, vector<vector<int>>& edges) {
-        //1st create AdjList
-        unordered_map<int, vector<int>> AdjList;
-        for(vector<int> edge: edges){
-            int u = edge[0];
-            int v = edge[1];
-            //u->v (directed)
-            AdjList[u].push_back(v);
-        }
-        //lets start the process now
-        vector<int> ans;
-        solveBFS(V, edges, AdjList, ans);
-        if(ans.size() == V)return false;
-        if(ans.size() < V) return true;
-        
+        return false;
     }
 };
