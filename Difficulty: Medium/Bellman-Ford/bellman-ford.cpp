9d05315106d016b81fe583aa03c2a6e1113c9281
Sairudra More <sairudra60@gmail.com>
2026-04-19 13:32:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -1,54 +1,64 @@
-//
+// User function Template for C++
 
 class Solution {
   public:
     vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
-        const long long INF = 1e8;
-        //create adjList
+        vector<int> distAns(V, 1e8);
+        distAns[src] = 0;
+        
+        //adjList
         unordered_map<int, vector<pair<int, int>>> adjList;
         for(vector<int> edge: edges){
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
-            //directed edges
+            //directed
+            //u->v
             adjList[u].push_back({w, v});
         }
         
-        //initialise
-        vector<int>dist(V, INF);
-        dist[src] = 0;
-        
-        //loop for i<n-1 times to get the shortest path
-        for(int i = 0; i < V - 1; i++){
-            for(pair<const int, vector<pair<int, int>>> a: adjList){ 
-                int node = a.first; //nbr = a.second 
-                for(pair<int, int> nbr: a.second){
-                    int nbrDist = nbr.first;
-                    int nbrNode = nbr.second;
-                    if(dist[node] != INF && dist[node] + nbrDist < dist[nbrNode]){
-                        dist[nbrNode] = dist[node] + nbrDist;
+        //iterate V-1 times
+        for(int i=0; i<V-1; i++){
+            //traverse through the adjList
+            for(pair<const int, vector<pair<int, int>>> a: adjList){
+                int u = a.first;
+                if(distAns[u] == 1e8){
+                    continue;
+                }
+                vector<pair<int, int>> data = a.second;
+                //explore the nbrs
+                for(pair<int, int> pairData: data){
+                    int nbrWt = pairData.first;
+                    int nbrNode = pairData.second;
+                    if(distAns[u] + nbrWt < distAns[nbrNode]){
+                        distAns[nbrNode] = distAns[u] + nbrWt;
                     }
                 }
             }
         }
         
-        //do 1 last iteration to find cycle
-        bool negativeCycle = false;
-        for(int i = 0; i < V - 1; i++){
+        //iterate 1 last time to check if there is a - loop
+        do{
+            //traverse through the adjList
             for(pair<const int, vector<pair<int, int>>> a: adjList){
-                int node = a.first; //nbr = a.second
-                for(pair<int, int> nbr: a.second){
-                    int nbrDist = nbr.first;
-                    int nbrNode = nbr.second;
-                    if(dist[node] != INF && dist[node] + nbrDist < dist[nbrNode]){
-                        negativeCycle = true;
+                int u = a.first;
+                if(distAns[u] == 1e8){
+                    continue;
+                }
+                vector<pair<int, int>> data = a.second;
+                //explore the nbrs
+                for(pair<int, int> pairData: data){
+                    int nbrWt = pairData.first;
+                    int nbrNode = pairData.second;
+                    if(distAns[u] + nbrWt < distAns[nbrNode]){
+                        //- loop, not possible to find shortest path
                         return {-1};
                     }
                 }
             }
-        }
+        }while(false);
         
-        return dist;
+        return distAns;
         
     }
 };
