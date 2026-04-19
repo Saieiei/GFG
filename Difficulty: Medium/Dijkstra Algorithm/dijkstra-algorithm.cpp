30741a22ecac5fa1b09eb2b620c828acb3ae56d4
Sairudra More <sairudra60@gmail.com>
2026-04-19 12:46:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,51 +1,50 @@
-//we require set and dis[] and adjList
-//easy only
+//set method
+//O(v + u (log v))
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        //create adjList
-        unordered_map<int, vector<pair<int, int>>> adjList; //u, w, v
+        //adjList
+        unordered_map<int, vector<pair<int, int>>> adjList;
         for(vector<int> edge: edges){
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
             //undirected
+            //u<->v
             adjList[u].push_back({w, v});
             adjList[v].push_back({w, u});
         }
+        
         set<pair<int, int>> st; //w, u
-        vector<int>dist (V, INT_MAX);
-        //initial case
-        dist[src] = 0;
         st.insert({0, src});
-        //start the process
+        vector<int> distAns(V, INT_MAX);
+        distAns[src] = 0;
+        
+        //
         while(!st.empty()){
-            pair<int, int> frontPair =  *(st.begin());
-            //w, u
-            int nodeDist = frontPair.first;
-            int node = frontPair.second;
-            //after u take element from the set pop it
-            st.erase(st.begin());
-            //process its nbrs
-            for(pair<int, int> nbr: adjList[node]){
-                int nbrDist = nbr.first;
+            pair<int, int> frontPair = *st.begin();
+            st.erase(*st.begin());
+            int Node = frontPair.second;
+            int NodeWt = frontPair.first;
+            //explore its nbrs, if the wt is less in dist then 
+            //it will be taken care of by removing it from st
+            //and not processing it from the next next part
+            for(pair<int, int> nbr: adjList[Node]){
                 int nbrNode = nbr.second;
-                //check if smaller in dist vector
-                if(dist[node] + nbrDist < dist[nbrNode]){
-                    //we will delete nbrs occurance if found and update and insert it back in st
-                    auto result = st.find({dist[nbrNode], nbrNode});
-                    if(result != st.end()){
-                        //found it and pop it
-                        st.erase(result);
+                int nbrWt = nbr.first;
+                if(distAns[Node] + nbrWt < distAns[nbrNode]){
+                    //remove if the bigger 1 is found in the st
+                    auto it = st.find({distAns[nbrNode], nbrNode});
+                    if(it != st.end()){//found it
+                        st.erase(it);
                     }
-                    //update the dist and push the pair in st 
-                    dist[nbrNode] = dist[node] + nbrDist;
-                    st.insert({dist[nbrNode], nbrNode});
+                    //normal part, where u update the distAns[]
+                    //and push the updated pair in the st, to keep the loop going
+                    distAns[nbrNode] = distAns[Node] + nbrWt;
+                    st.insert({distAns[nbrNode], nbrNode});
                 }
             }
         }
-        return dist;
-        
-        
+        return distAns;
     }
 };
