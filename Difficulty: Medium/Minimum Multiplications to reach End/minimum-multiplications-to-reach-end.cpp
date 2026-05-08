c06e375a2c7bcba624d236ac8f1902a30a9cf0a4
Sairudra More <sairudra60@gmail.com>
2026-05-08 21:53:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -1,37 +1,45 @@
 //BFS
 class Solution {
   public:
-    int minimumMultiplications(vector<int>& arr, int start, int end) {
-        queue<pair<int, int>> q;
-        vector<bool> isVisisted(100000, false);
+    int minSteps(vector<int>& arr, int start, int end) {
+        //bc
+        if(start == end){
+            return 0;
+        }
+        //no need of adjList
+        const int module = 1000;
+        //we cant predict this, but its suppose to be high
+        vector<bool> isVisited(100000, false);
         int steps = 0;
-        q.push({start, steps});
-        isVisisted[start] == true;
-        const int mod = 100000;
         
+        //BFS
+        queue<pair<int, int>> q;
+        q.push({start, steps});
+        isVisited[start] = true;
+        
+        //start the process
         while(!q.empty()){
-            pair<int, int> frontEles = q.front();
+            pair<int, int> frontPair = q.front();
             q.pop();
-            int node = frontEles.first;
-            int steps = frontEles.second;
-            //check
-            if(node == end){
-                return steps;
-            }
-            //if not explore nbrs/multiplications
-            for(int mulValues: arr){
-                int mulValueNodes = (node * mulValues) % mod;
-                int newStepValue = steps + 1;
-                //push in node if not visited and mark them as visited immediately
-                if(!isVisisted[mulValueNodes]){
-                    isVisisted[mulValueNodes] = true;
-                    q.push({mulValueNodes, newStepValue});
+            int frontNumber = frontPair.first;
+            int frontSteps = frontPair.second;
+            //explore nbrs
+            for(int multiplierNbr : arr){
+                int nbrValue = (multiplierNbr * frontNumber) % module;
+                //check if this is already visisted
+                if(!isVisited[nbrValue]){
+                    int nbrSteps = frontSteps + 1;
+                    //u will stop when u have reached the end
+                    if(nbrValue == end){
+                        return nbrSteps;
+                    }
+                    q.push({nbrValue, nbrSteps});
+                    isVisited[nbrValue] = true;
                 }
             }
-            
         }
         //not possible
-        return  -1;
+        return -1;
         
     }
 };
