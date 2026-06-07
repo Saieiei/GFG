eb0ecb48be7d7bc5a6300532ba322172ed60f2a8
Sairudra More <sairudra60@gmail.com>
2026-06-07 09:55:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.cpp
@@ -3,14 +3,35 @@ class Solution {
   public:
     int findTarget(vector<int>& arr, int target) {
         int n = arr.size();
-       //brute force
-       //linear search O(N)
-       for(int i=0; i<n; i++){
-           if(arr[i] == target){
-               return i;
-           }
-       }
-       return -1;
-        
+        //Binary search LogN
+        //since its nearly sorted we have to check 
+        //its 1 left and 1 right as well
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            //check if we have reached the target
+            if(arr[mid] == target){
+                return mid;
+            }
+            //since its nearly sorted, 
+            //check its left and right once
+            if(mid + 1 < n && arr[mid + 1] == target){
+                return mid + 1;
+            }
+            else if(mid-1 >=0 && arr[mid-1] == target){
+                return mid - 1;
+            }
+            else{
+                //consider the fact that u check 2 elements
+                if(target > arr[mid]){
+                    low = mid + 2;
+                }
+                else{
+                    high = mid - 2;
+                }
+            }
+        }
+        return -1;
     }
 };
